df9da508bdf14aa9f69de69f08239b79f593a578
Mowie <mowrer.sam@gmail.com>
2020-02-06 21:02:35 -0600

    Fixed issue where incorrect path to .ico file crashed the application.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,6 +19,8 @@ namespace daily_dilbert
         private void SetAsWallpaper_Click(object sender, RoutedEventArgs e)
         {
             // Set as wallpaper. Work in progress.
+
+            saveFileDialog.ShowDialog();
         }
 
         private async void Window_Loaded(object sender, RoutedEventArgs e)
