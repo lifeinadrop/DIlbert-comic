ab2a5ee42e6c30fbab477a14e9dc2ab91cae7bca
Mowie <mowrer.sam@gmail.com>
2020-02-06 22:53:08 -0600

    Added saving functionality to users Pictures directory.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows;
@@ -16,19 +19,58 @@ namespace daily_dilbert
             InitializeComponent();
         }
 
-        private void SetAsWallpaper_Click(object sender, RoutedEventArgs e)
+        private void SaveComic_Click(object sender, RoutedEventArgs e)
         {
-            // Set as wallpaper. Work in progress.
+            try
+            {
+                string todaysDate = DateTime.Today.ToString("yyyy-MM-dd");
 
-            saveFileDialog.ShowDialog();
+                // Check to see if the file already exists. If it does, add a random number to the end. This isn't necessary, this was more just a learning exercise for me.
+                if (!System.IO.File.Exists(Path(todaysDate + ".png")))
+                {
+                    
+                    String filePath = Path(todaysDate + ".png");
+
+                    var encoder = new PngBitmapEncoder();
+                    encoder.Frames.Add(BitmapFrame.Create((BitmapSource)comic.Source));
+                    using (FileStream stream = new FileStream(filePath, FileMode.Create))
+                        encoder.Save(stream);
+
+                    MessageBox.Show("Strip saved to Pictures folder.");
+                }
+                else
+                {
+                    Random random = new Random();
+                    int randomNum = random.Next(1000, 10000);
+
+                    String filePath = Path(todaysDate + "_" + randomNum + ".png");
+
+                    var encoder = new PngBitmapEncoder();
+                    encoder.Frames.Add(BitmapFrame.Create((BitmapSource)comic.Source));
+                    using (FileStream stream = new FileStream(filePath, FileMode.Create))
+                        encoder.Save(stream);
+
+                    MessageBox.Show("Strip saved to Pictures folder.");
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Picture did not save.");
+                Console.Write(ex.Message);
+            }
+            
+            
         }
 
+        /// <summary>
+        /// Generate the HTTP request on application load and set the Image box source to the returned bitmap image.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            // Get today's date and format it the way the url has the date formatted.
-            string todaysDate = DateTime.Today.ToString("yyyy-MM-dd");
-            string stripUrl = "https://dilbert.com/strip/" + todaysDate;
-            string assetUrl = await GetHtml(new Uri(stripUrl));
+            string assetUrl = await GetHtml(new Uri(FormatUrl()));
 
             var bi3 = new BitmapImage();
             bi3.BeginInit();
@@ -67,5 +109,30 @@ namespace daily_dilbert
                 return null;
             }
         }
+
+        /// <summary>
+        /// Method that handles formatting the URL with the current date.
+        /// </summary>
+        /// <returns>Returns a formatted string to use as a URL.</returns>
+
+        private string FormatUrl()
+        {
+            string todaysDate = DateTime.Today.ToString("yyyy-MM-dd");
+            string stripUrl = "https://dilbert.com/strip/" + todaysDate;
+
+            return stripUrl;
+        }
+
+        /// <summary>
+        /// Get the path to the users Pictures directory.
+        /// </summary>
+        /// <param name="target"></param>
+        /// <returns>Returns formatted path to the correct directory.</returns>
+        public static string Path(string target)
+        {
+            string basePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Pictures\";
+            return basePath + target;
+        }
+
     }
 }
